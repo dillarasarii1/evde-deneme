2c869048d8234a95f3da02e57b1cb18786f9e544
dillarasarii1 <146071726+dillarasarii1@users.noreply.github.com>
2025-03-02 16:47:29 +0300

    Add files via upload

---
+++ proj.ev/aritmetik.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Birinci sayıyı gir:"
+read sayi1
+echo "İkinci sayıyı gir:"
+read sayi2
+echo "Toplam: $((sayi1 + sayi2))"
---
+++ proj.ev/isimSor.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "isminizi yazin:"
+read ad
